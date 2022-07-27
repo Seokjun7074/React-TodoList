84e485a1021c22b635d81c9f1f645019b1efaf73
seokjun <sukjune96@gmail.com>
2022-07-27 18:57:18 +0900

    ref를 이용해 input값 가져오는 방식

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,28 +2,43 @@ import { useRef, useState } from "react";
 import "./style.css";
 const Form = ({ todo, setTodo }) => {
   const nextId = useRef(0); // 고유아이디 useRef는 값이 변해도 리렌더 안함
-  const [inputs, setInputs] = useState({
-    id: nextId.current,
-    title: "",
-    content: "",
-    isDone: false,
-  });
-  const { title, content } = inputs;
-  const onChangeHandler = (e) => {
-    setInputs({
-      id: nextId.current,
-      ...inputs,
-      [e.target.name]: e.target.value,
-      isDone: false,
-    });
-  };
+  const ref_title = useRef();
+  const ref_content = useRef();
+  // const [inputs, setInputs] = useState({
+  //   id: nextId.current,
+  //   title: "",
+  //   content: "",
+  //   isDone: false,
+  // });
+  // const { title, content } = inputs;
+  // const onChangeHandler = (e) => {
+  //   console.log(e.target.name);
+  //   setInputs({
+  //     id: nextId.current,
+  //     ...inputs,
+  //     [e.target.name]: e.target.value,
+  //     isDone: false,
+  //   });
+  // };
 
   const onSubmitHandler = () => {
-    setTodo([...todo, inputs]);
-    setInputs({
-      title: "",
-      content: "",
-    });
+    // 현재 input에 있는 값 가지고 오는 부분
+    let titleValue = ref_title.current.value;
+    let contentValue = ref_content.current.value;
+    // 새로 만들어줄 객체 생성
+    const newData = {
+      id: nextId.current,
+      title: titleValue,
+      content: contentValue,
+      isDone: false,
+    };
+    // 투두리스트 목록에 넣어줌
+    setTodo([...todo, newData]);
+
+    // 빈칸으로 만들어주는 부분
+    ref_title.current.value = "";
+    ref_content.current.value = "";
+    // 고유 아이디 1증가
     nextId.current++;
   };
   return (
@@ -33,15 +48,17 @@ const Form = ({ todo, setTodo }) => {
         <input
           type="text"
           name="title"
-          onChange={onChangeHandler}
-          value={title}
+          // onChange={onChangeHandler}
+          // value={title}
+          ref={ref_title}
         ></input>
         <label>내용</label>
         <input
           type="text"
           name="content"
-          onChange={onChangeHandler}
-          value={content}
+          // onChange={onChangeHandler}
+          // value={content}
+          ref={ref_content}
         ></input>
       </div>
       <button className="add_button" onClick={onSubmitHandler}>
